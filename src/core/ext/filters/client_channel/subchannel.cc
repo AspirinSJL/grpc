95b92f0502488e66807941ababbf9946cfd6d050
Juanli Shen <juanlishen@google.com>
2019-01-30 11:30:47 -0800

    Unref watcher after releasing lock

--- src/core/ext/filters/client_channel/subchannel.cc
+++ src/core/ext/filters/client_channel/subchannel.cc
@@ -304,18 +304,22 @@ class ConnectedSubchannelStateWatcher
   static void OnHealthChanged(void* arg, grpc_error* error) {
     auto* self = static_cast<ConnectedSubchannelStateWatcher*>(arg);
     grpc_subchannel* c = self->subchannel_;
-    MutexLock lock(&c->mu);
-    if (self->health_state_ == GRPC_CHANNEL_SHUTDOWN) {
-      self->Unref();
-      return;
+    {
+      MutexLock lock(&c->mu);
+      if (self->health_state_ != GRPC_CHANNEL_SHUTDOWN) {
+        if (self->last_connectivity_state_ == GRPC_CHANNEL_READY) {
+          grpc_connectivity_state_set(&c->state_and_health_tracker,
+                                      self->health_state_,
+                                      GRPC_ERROR_REF(error), "health_changed");
+        }
+        self->health_check_client_->NotifyOnHealthChange(
+            &self->health_state_, &self->on_health_changed_);
+        self = nullptr;  // So we don't unref below.
+      }
     }
-    if (self->last_connectivity_state_ == GRPC_CHANNEL_READY) {
-      grpc_connectivity_state_set(&c->state_and_health_tracker,
-                                  self->health_state_, GRPC_ERROR_REF(error),
-                                  "health_changed");
-    }
-    self->health_check_client_->NotifyOnHealthChange(&self->health_state_,
-                                                     &self->on_health_changed_);
+    // Don't unref until we've released the lock, because this might
+    // cause the subchannel (which contains the lock) to be destroyed.
+    if (self != nullptr) self->Unref();
   }
 
   grpc_subchannel* subchannel_;
